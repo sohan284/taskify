89b6dfe2ea08da8ce9208008cc5f521392e1667b
Sohanur <sr.sohan088@gmail.com>
2024-09-05 16:08:34 +0600

    add nav

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import SideBar from '../shared/SideBar';
+import { Outlet } from 'react-router-dom';
+
+
+const HomePage = () => {
+
+    return (
+        <div>
+            <SideBar />
+            <Outlet />
+        </div>
+    );
+};
+
+export default HomePage;

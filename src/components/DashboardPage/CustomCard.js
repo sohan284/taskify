18babcdce7f7d53e59ae5b63617b51d297fc6eb6
sohanur <sr.sohan088@gmail.com>
2024-09-06 09:44:57 +0600

    set new

--- src/components/DashboardPage/CustomCard.js
+++ src/components/DashboardPage/CustomCard.js
@@ -5,7 +5,7 @@ function CustomCard({ title }) {
         <div className="shadow-xl p-5 rounded-lg">
             <ArrowForwardIcon />
             <p>{title}</p>
-            <h2 className='text-gray-500 mt-3 text-[16px] font-semibold'>Total </h2>
+            <h2 className='text-gray-500 mt-3 text-[16px] font-semibold'>Total Task</h2>
             <p className='text-4xl font my-2 medium text-gray-600'>45</p>
             <button>See More <ArrowForwardIcon /></button>
         </div>
